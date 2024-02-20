1131122677cc537e166d30f7220022bdb4b85362
Saotzuri <133770938+Saotzuri@users.noreply.github.com>
2024-02-20 01:40:28 +0100

    wokring mob tracker hud

--- example.js
+++ example.js
@@ -1,35 +1,26 @@
 import settings from "./settings";
+import { registerWhen } from "./utils/variables";
 
-
-register("command", DianaMobTrackerToggle).setName("showgui")
+registerWhen(register("RenderOverlay", DianaMobTrackerToggle), () => settings.dianaMobTracker);
 
 
 
 function DianaMobTrackerToggle() {
-    if(settings.dianaMobTracker) {
+    if (settings.dianaMobTracker) {
         SimpleDisplay.show();
-    }
-    else if(!settings.dianaMobTracker) {
+    } else {
         SimpleDisplay.hide();
     }
-    
-}
-
-
-var mygui = new Gui()
-mygui.registerDraw(drawMyGui)
-function drawMyGui() {
-    Renderer.drawRect(
-        0xffff0000,
-        Renderer.screen.getWidth() / 2 - 50,
-        Renderer.screen.getHeight() / 2 - 50,
-        100, 100
-    )
 }
 
 var SimpleDisplay = new Display();
 SimpleDisplay.hide();
 SimpleDisplay.setBackground("full");
 SimpleDisplay.setRenderLoc(150,150);
-SimpleDisplay.setLine(0, "&cDiana Mob Kills");
-SimpleDisplay.setLine(1, "&cInquis: ");
+SimpleDisplay.setLine(0, "&4Diana Mob Kills");
+SimpleDisplay.setLine(1, "Minos Inquisitor: ");
+SimpleDisplay.setLine(2, "Minos Champion: ");
+SimpleDisplay.setLine(3, "Minotaur: ");
+SimpleDisplay.setLine(4, "Gaia Construct: ");
+SimpleDisplay.setLine(5, "Siamese Lynx: ");
+SimpleDisplay.setLine(6, "Minos Hunter: ");
