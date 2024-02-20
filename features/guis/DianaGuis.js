d04f9f08cd57e64ee24a1639fdc861c6399ee24a
Saotzuri <133770938+Saotzuri@users.noreply.github.com>
2024-02-20 21:00:40 +0100

    Update Diana Guis with new colors and formatting

--- features/guis/DianaGuis.js
+++ features/guis/DianaGuis.js
@@ -3,6 +3,7 @@ import { registerWhen } from "../../utils/variables";
 import { getWorld } from "../../utils/world";
 import {  state } from "../../utils/functions";
 import { Overlay } from "../../utils/Overlay";
+import { YELLOW, BOLD, GOLD, DARK_GREEN, LIGHT_PURPLE, DARK_PURPLE, GREEN, DARK_GRAY, GRAY} from "../../utils/constants";
 
 registerWhen(register("entityDeath", () => {
     state.entityDeathOccurred = true;
@@ -12,30 +13,36 @@ registerWhen(register("entityDeath", () => {
 }), () => getWorld() === "Hub" && settings.dianaMobTracker);
 
 dianaMobTrackerExample = 
-`&4Diana Mob Tracker
-Minos Inquisitor: 
-Minos Champion:
-Minotaur:
-Gaia Construct:
-Siamese Lynx:
-Minos Hunter:
-Total Mobs:
+`${YELLOW}${BOLD}Diana Mob Tracker
+------------------
+${LIGHT_PURPLE}${BOLD}Minos Inquisitor: 
+${DARK_PURPLE}${BOLD}Minos Champion:
+${GOLD}${BOLD}Minotaur:
+${GREEN}${BOLD}Gaia Construct:
+${GREEN}${BOLD}Siamese Lynx:
+${GREEN}${BOLD}Minos Hunter:
+${GRAY}${BOLD}Total Mobs:
 `
 dianaLootTrackerExample = 
-`&4Diana Loot Tracker
-Griffin Feather:
-Chimera:
-Minos Relic:
-Daedalus Stick:
-Dwarf Turtle Shelmet:
-Crochet Tiger Plushies:
-Antique Remedies:
-Crown of Greed:
-Washed-up Souvenir:
-Coins:
+`${YELLOW}${BOLD}Diana Loot Tracker
+-------------------
+${LIGHT_PURPLE}${BOLD}Chimera:
+${DARK_PURPLE}${BOLD}Minos Relic:
+${GOLD}${BOLD}Griffin Feather:
+${GOLD}${BOLD}Daedalus Stick:
+${GOLD}${BOLD}Crown of Greed:
+${GOLD}${BOLD}Washed-up Souvenir:
+${DARK_GREEN}${BOLD}Dwarf Turtle Shelmet:
+${DARK_GREEN}${BOLD}Crochet Tiger Plushies:
+${DARK_GREEN}${BOLD}Antique Remedies:
+${GRAY}${BOLD}Enchanted Gold: 
+${GRAY}${BOLD}Enchanted Iron: 
+${GRAY}${BOLD}Enchanted Ancient Claw: 
+${GRAY}${BOLD}Ancient Claw: 
+${GOLD}${BOLD}Coins:
 `
 const DianaMobTracker = new Overlay("dianaMobTrackerView",["Hub"], [10,50,1],"moveMobCoounter",dianaMobTrackerExample,"dianaMobTracker");
-const DianaLootTracker = new Overlay("dianaLootTrackerView",["Hub"], [10,130,1],"moveLootCoounter",dianaLootTrackerExample,"dianaLootTracker");
+const DianaLootTracker = new Overlay("dianaLootTrackerView",["Hub"], [10,150,1],"moveLootCoounter",dianaLootTrackerExample,"dianaLootTracker");
 
 /**
  * 
@@ -44,13 +51,23 @@ const DianaLootTracker = new Overlay("dianaLootTrackerView",["Hub"], [10,130,1],
 export function refreshMobOverlay(mobTracker, setting) {
     if(setting > 0){
     DianaMobTracker.message =
+    `${YELLOW}${BOLD}Diana Mob Tracker
+------------------
+${LIGHT_PURPLE}${BOLD}Minos Inquisitor: ${mobTracker["mobs"]["Minos Inquisitor"]}
+${DARK_PURPLE}${BOLD}Minos Champion: ${mobTracker["mobs"]["Minos Champion"]}
+${GOLD}${BOLD}Minotaur: ${mobTracker["mobs"]["Minotaur"]}
+${GREEN}${BOLD}Gaia Construct: ${mobTracker["mobs"]["Gaia Construct"]}
+${GREEN}${BOLD}Siamese Lynx: ${mobTracker["mobs"]["Siamese Lynx"]}
+${GREEN}${BOLD}Minos Hunter: ${mobTracker["mobs"]["Minos Hunter"]}
+${GRAY}${BOLD}Total Mobs:
+`
     `&4Diana Mob Tracker
-Minos Inquisitor: ${mobTracker["mobs"]["Minos Inquisitor"]}
-Minos Champion: ${mobTracker["mobs"]["Minos Champion"]}
-Minotaur: ${mobTracker["mobs"]["Minotaur"]}
-Gaia Construct: ${mobTracker["mobs"]["Gaia Construct"]}
-Siamese Lynx: ${mobTracker["mobs"]["Siamese Lynx"]}
-Minos Hunter: ${mobTracker["mobs"]["Minos Hunter"]}
+Minos Inquisitor: ${GRAY}${mobTracker["mobs"]["Minos Inquisitor"]}
+Minos Champion: ${GRAY}${mobTracker["mobs"]["Minos Champion"]}
+Minotaur: ${GRAY}${mobTracker["mobs"]["Minotaur"]}
+Gaia Construct: ${GRAY}${mobTracker["mobs"]["Gaia Construct"]}
+Siamese Lynx: ${GRAY}${mobTracker["mobs"]["Siamese Lynx"]}
+Minos Hunter: ${GRAY}${mobTracker["mobs"]["Minos Hunter"]}
 Total Mobs: 
 `
     }
@@ -61,22 +78,23 @@ Total Mobs:
  */
 export function refreshItemOverlay(lootTracker, setting){
     if(setting > 0){
-    DianaLootTracker.message = 
-    `&4Diana Loot Tracker
-Griffin Feather: ${lootTracker["items"]["Griffin Feather"]}
-Chimera: ${lootTracker["items"]["Chimera"]}
-Minos Relic: ${lootTracker["items"][""]}
-Daedalus Stick: ${lootTracker["items"]["Daedalus Stick"]}
-Dwarf Turtle Shelmet: ${lootTracker["items"][""]}
-Crochet Tiger Plushies: ${lootTracker["items"][""]}
-Antique Remedies: ${lootTracker["items"][""]}
-Crown of Greed: ${lootTracker["items"]["Crown of Greed"]}
-Washed-up Souvenir: ${lootTracker["items"]["Washed-up Souvenir"]}
-Enchanted Gold: ${lootTracker["items"]["ENCHANTED_GOLD"]}
-Enchanted Iron: ${lootTracker["items"]["ENCHANTED_IRON"]}
-Enchanted Ancient Claw: ${lootTracker["items"]["ENCHANTED_ANCIENT_CLAW"]}
-Ancient Claw: ${lootTracker["items"]["ANCIENT_CLAW"]}
-Coins: ${lootTracker["items"]["coins"]}
+    DianaLootTracker.message =
+    `${YELLOW}${BOLD}Diana Loot Tracker
+-------------------
+${LIGHT_PURPLE}${BOLD}Chimera: ${GRAY}${lootTracker["items"]["Chimera"]}
+${DARK_PURPLE}${BOLD}Minos Relic: ${GRAY}${lootTracker["items"][""]}
+${GOLD}${BOLD}Griffin Feather: ${GRAY}${lootTracker["items"]["Griffin Feather"]}
+${GOLD}${BOLD}Daedalus Stick: ${GRAY}${lootTracker["items"]["Daedalus Stick"]}
+${GOLD}${BOLD}Crown of Greed: ${GRAY}${lootTracker["items"]["Crown of Greed"]}
+${GOLD}${BOLD}Washed-up Souvenir: ${GRAY}${lootTracker["items"]["Washed-up Souvenir"]}
+${DARK_GREEN}${BOLD}Dwarf Turtle Shelmet: ${GRAY}${lootTracker["items"][""]}
+${DARK_GREEN}${BOLD}Crochet Tiger Plushies: ${GRAY}${lootTracker["items"][""]}
+${DARK_GREEN}${BOLD}Antique Remedies: ${GRAY}${lootTracker["items"][""]}
+${GRAY}${BOLD}Enchanted Gold: ${GRAY}${lootTracker["items"]["ENCHANTED_GOLD"]}
+${GRAY}${BOLD}Enchanted Iron: ${GRAY}${lootTracker["items"]["ENCHANTED_IRON"]}
+${GRAY}${BOLD}Enchanted Ancient Claw: ${GRAY}${lootTracker["items"]["ENCHANTED_ANCIENT_CLAW"]}
+${GRAY}${BOLD}Ancient Claw: ${GRAY}${lootTracker["items"]["ANCIENT_CLAW"]}
+${GOLD}${BOLD}Coins: ${GRAY}${lootTracker["items"]["coins"]}
 `
     }
 }
