b0dc9326853d80c41e7652f68d0f709d11303b4f
Saotzuri <133770938+Saotzuri@users.noreply.github.com>
2024-02-20 01:43:52 +0100

    working tracker without moving

--- example.js
+++ example.js
@@ -6,6 +6,18 @@ registerWhen(register("RenderOverlay", DianaMobTrackerToggle), () => settings.di
 
 
 function DianaMobTrackerToggle() {
+    var SimpleDisplay = new Display();
+    SimpleDisplay.hide();
+    SimpleDisplay.setBackground("full");
+    SimpleDisplay.setBackgroundColor(0x00000000);
+    SimpleDisplay.setRenderLoc(150,150);
+    SimpleDisplay.setLine(0, "&4Diana Mob Kills");
+    SimpleDisplay.setLine(1, "Minos Inquisitor: ");
+    SimpleDisplay.setLine(2, "Minos Champion: ");
+    SimpleDisplay.setLine(3, "Minotaur: ");
+    SimpleDisplay.setLine(4, "Gaia Construct: ");
+    SimpleDisplay.setLine(5, "Siamese Lynx: ");
+    SimpleDisplay.setLine(6, "Minos Hunter: ");
     if (settings.dianaMobTracker) {
         SimpleDisplay.show();
     } else {
@@ -13,14 +25,4 @@ function DianaMobTrackerToggle() {
     }
 }
 
-var SimpleDisplay = new Display();
-SimpleDisplay.hide();
-SimpleDisplay.setBackground("full");
-SimpleDisplay.setRenderLoc(150,150);
-SimpleDisplay.setLine(0, "&4Diana Mob Kills");
-SimpleDisplay.setLine(1, "Minos Inquisitor: ");
-SimpleDisplay.setLine(2, "Minos Champion: ");
-SimpleDisplay.setLine(3, "Minotaur: ");
-SimpleDisplay.setLine(4, "Gaia Construct: ");
-SimpleDisplay.setLine(5, "Siamese Lynx: ");
-SimpleDisplay.setLine(6, "Minos Hunter: ");
+
