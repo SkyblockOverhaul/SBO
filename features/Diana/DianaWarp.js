63b3a7ee26c9bb70dd724ae85055ceb5342ca6dd
RolexDE <133770982+RolexDE@users.noreply.github.com>
2024-02-23 02:12:18 +0100

    Refactor warps object to hubWarps object in DianaWarp.js

--- features/Diana/DianaWarp.js
+++ features/Diana/DianaWarp.js
@@ -1,13 +1,11 @@
 import { getInqWaypoints } from "./../general/Waypoints";
 
-
-let warps = {
+let hubWarps = {
     castle: {x: -250, y: 130, z: 45, unlocked: true},
     da: {x: 92, y: 75, z: 174, unlocked: true},
     hub: {x: -3, y: 70, z: 70, unlocked: true},
     museum: {x: -76, y: 76, z: 81, unlocked: true},
 };
-
 
 const inquisWarpKey = new KeyBind("Iqnuis Warp", Keyboard.KEY_NONE, "SkyblockOverhaul");
 let tryWarp = false;
@@ -25,27 +23,25 @@ inquisWarpKey.registerKeyPress(() => {
 
 register("chat", () => {
     if (tryWarp) {
-        ChatLib.chat(closestWarp + " is not unlocked!")
-        warps[closestWarp].unlocked = false;
+        ChatLib.chat("§6[SBO] §4" + closestWarp + " is not unlocked!")
+        hubWarps[closestWarp].unlocked = false;
     }
 }).setCriteria("&r&cYou haven't unlocked this fast travel destination!&r");
 
 let closestWarp = undefined;
 function getClosestWarp(x,y,z){
-
     let closestDistance = Infinity;
-    
-    for (let warp in warps) {
-        if (warps[warp].unlocked) continue;
-        let distance = Math.sqrt(
-            (warps[warp].x - x)**2 +
-            (warps[warp].y - y)**2 +
-            (warps[warp].z - z)**2
-        );
-
-        if (distance < closestDistance) {
-            closestDistance = distance;
-            closestWarp = warp;
+    for (let warp in hubWarps) {
+        if (hubWarps[warp].unlocked){
+            let distance = Math.sqrt(
+                (hubWarps[warp].x - x)**2 +
+                (hubWarps[warp].y - y)**2 +
+                (hubWarps[warp].z - z)**2
+            );
+            if (distance < closestDistance) {
+                closestDistance = distance;
+                closestWarp = warp;
+            }
         }
     }
 }
