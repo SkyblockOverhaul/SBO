bb71ab0a5aa56180d1a063a3769533ae7ff04b33
Saotzuri <133770938+Saotzuri@users.noreply.github.com>
2024-02-24 18:23:11 +0100

    Fix import path and add switch case for EntityCrit2FX

--- features/Diana/DianaBurrowDetect.js
+++ features/Diana/DianaBurrowDetect.js
@@ -1,11 +1,19 @@
-import settings from "../settings.js";
+import settings from "../../settings.js";
 
-register("spawnParticle", (particle, type, event) => {
+register("step", (particle, type, event) => {
     burrowDetect(particle);
-});
+}).setFps(1);
 
 
-function burrowDetect(particle) {
+function burrowDetect(particle, type) {
+    const particlepos = particle.getPos();
+    const xyz = [particlepos.getX(), particlepos.getY(), particlepos.getZ()];
+    const [x, y, z] = [xyz[0], xyz[1], xyz[2]];
     const type = particle.toString();
     print(type);
+    switch (type) {
+        case "EntityCrit2FX":
+            print("Crit");
+            break;
+    }
 }
