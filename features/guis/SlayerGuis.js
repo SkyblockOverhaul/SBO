40762f8e615d988638682bc886ab1b1a371a92d7
Saotzuri <133770938+Saotzuri@users.noreply.github.com>
2024-03-11 21:16:45 +0100

    Add GUI examples for Diana and Slayer loot trackers

--- features/guis/SlayerGuis.js
+++ features/guis/SlayerGuis.js
@@ -4,34 +4,9 @@ import { getWorld } from "../../utils/world";
 import { loadGuiSettings, saveGuiSettings } from "../../utils/functions";
 import { Overlay } from "../../utils/overlay";
 import { YELLOW, BOLD, GOLD, DARK_GREEN, LIGHT_PURPLE, DARK_PURPLE, GREEN, DARK_GRAY, GRAY, WHITE, AQUA, ITALIC, BLUE} from "../../utils/constants";
+import { blazeLootTrackerExample } from "../../utils/guiExamples";
 import {  } from "../slayer/SlayerDropDetect";
 
-
-let blazeLootTrackerExample = 
-`${YELLOW}${BOLD}Blaze Loot Tracker
--------------------
-${WHITE}${BOLD}Ice-Flavored:
-${WHITE}${BOLD}Fire Aspect III:
-${GREEN}${BOLD}Flawed Opal Gems:
-${BLUE}${BOLD}Lavatears Runes:
-${BLUE}${BOLD}Mana Disintegrator:
-${BLUE}${BOLD}Kelvin Inverter:
-${BLUE}${BOLD}Blaze Rod Dist:
-${BLUE}${BOLD}Glowstone Dist:
-${BLUE}${BOLD}Magma Cream Dist:
-${BLUE}${BOLD}Nether Wart Dist:
-${BLUE}${BOLD}Gabagool Dist:
-${DARK_PURPLE}${BOLD}Magma Arrows:
-${DARK_PURPLE}${BOLD}Archfiend Dice
-${GOLD}${BOLD}Fiery Burst Rune:
-${GOLD}${BOLD}Scorched Power:
-${GOLD}${BOLD}Engineering Plans:
-${GOLD}${BOLD}Subzero Inverter:
-${GOLD}${BOLD}High Class Dice:
-${LIGHT_PURPLE}${BOLD}Duplex:
-${LIGHT_PURPLE}${BOLD}Scorched Books:
-${GRAY}${BOLD}Blaze Killed: 
-`
 let guiSettings = loadGuiSettings();
 
 let BlazeLootTracker = new Overlay("blazeLootTrackerView",["Crimson Isle"], [10, 10, 0],"sbomoveBlazeCounter",blazeLootTrackerExample,"blazeLootTracker");
