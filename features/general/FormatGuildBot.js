e19ca4f47b8cc13f7abcb67847c475141911c519
RolexDE <133770982+RolexDE@users.noreply.github.com>
2024-03-23 20:47:46 +0100

    Fix guildbot message formatting

--- features/general/FormatGuildBot.js
+++ features/general/FormatGuildBot.js
@@ -1,15 +1,12 @@
 
 register("chat", (player, message, event) =>{
     // cancel original message
-    cancel(event);
     // send new guildbot message
     if (!player.includes(" ")) {
+        cancel(event);
         player = player.removeFormatting();
         ChatLib.chat("&r&2Guild > &b[DC] &b" + player + "&r:" + message);
         // print("&r&2Guild > &b[DC] &b" + player + "&r:" + message);
-    }
-    else {
-        print("contains space");
     }
 }).setCriteria("&r&2Guild > &a[VIP] SlowDT &3[GM]&f: ${player}:${message}").setContains()
 
