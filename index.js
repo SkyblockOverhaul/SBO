55143d0c1090c2c51d2f718b925a15bc646969e3
RolexDE <133770982+RolexDE@users.noreply.github.com>
2024-03-29 03:04:38 +0100

    Add todo comments for future improvements

--- index.js
+++ index.js
@@ -88,7 +88,12 @@ register("chat", (player, message, event) =>{
 //     }, 150);
 // }).setName("sboinq");
 
-// map size
+// todo
+// flasche gui clicks abfangen (maybe nach sound gehen)
+// alle anderen figuren einbauen
+// slots besser highlighten
+
+
 
 // all figures
 const clubbed1 = [{ 'x': 6, 'y': 0 }, { 'x': 7, 'y': 0 }, { 'x': 1, 'y': 1 }, { 'x': 6, 'y': 1 }, { 'x': 7, 'y': 1 }, { 'x': 0, 'y': 2 }, { 'x': 5, 'y': 2 }, { 'x': 1, 'y': 3 }, { 'x': 2, 'y': 3 }, { 'x': 3, 'y': 3 }, { 'x': 4, 'y': 3 }];
