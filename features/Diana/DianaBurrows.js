c1ae0d0d0181ba8af51697d8df241bb82944daeb
RolexDE <133770982+RolexDE@users.noreply.github.com>
2024-04-09 18:20:27 +0200

    Remove unused code in DianaBurrows.js

--- features/Diana/DianaBurrows.js
+++
@@ -1,7 +0,0 @@
-import settings from "../../settings";
-import { registerWhen } from "../../utils/variables";
-import { createBurrowWaypoints, removeBurrowWaypoint, setBurrowWaypoints, removeBurrowWaypointBySmoke } from "../general/Waypoints";
-import { getWorld } from "../../utils/world";
-import { checkDiana } from "../../utils/checkDiana";
-
-
