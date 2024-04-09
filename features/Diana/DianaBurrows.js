920d32cfd372426975ebeed08d8cf8f18687e0c9
RolexDE <133770982+RolexDE@users.noreply.github.com>
2024-04-09 18:25:48 +0200

    Revert "Remove unused code in DianaBurrows.js"
    This reverts commit c1ae0d0d0181ba8af51697d8df241bb82944daeb.

---
+++ features/Diana/DianaBurrows.js
@@ -0,0 +1,7 @@
+import settings from "../../settings";
+import { registerWhen } from "../../utils/variables";
+import { createBurrowWaypoints, removeBurrowWaypoint, setBurrowWaypoints, removeBurrowWaypointBySmoke } from "../general/Waypoints";
+import { getWorld } from "../../utils/world";
+import { checkDiana } from "../../utils/checkDiana";
+
+
