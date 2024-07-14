8f0782de2e40614f8c54b706cfa4e3e54230cdce
RolexDE <133770982+RolexDE@users.noreply.github.com>
2024-07-15 01:24:07 +0200

    refactor: Add loot share check in trackScavengerCoins function

--- features/Diana/DianaTracker.js
+++ features/Diana/DianaTracker.js
@@ -92,7 +92,7 @@ export function trackLootWithSacks(ammount, item) {
 
 let forbiddenCoins = [1, 5, 20, 1000, 2000, 3000, 4000, 5000, 7500, 8000, 10000, 12000, 15000, 20000, 25000, 40000, 50000]
 export function trackScavengerCoins(coins) {
-    if (mobDeath4SecsTrue()) {
+    if (mobDeath4SecsTrue() && gotLootShare()) {
         if (!forbiddenCoins.includes(coins) && coins < 60000) {
             trackItem("scavengerCoins", "items", coins);
             trackItem("coins", "items", coins);
