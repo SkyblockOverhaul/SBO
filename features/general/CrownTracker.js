f35d48d04b8da8ea169213f55de4874d1de065e0
Saotzuri <133770938+Saotzuri@users.noreply.github.com>
2024-08-08 23:23:57 +0200

    refactor: Improve ghost coin handling in CrownTracker

--- features/general/CrownTracker.js
+++ features/general/CrownTracker.js
@@ -197,7 +197,7 @@ function calculateGhostsTillTier() {
 
     let ghostCoinsAVG = ghostCoinsList.length > 0 ? ghostCoinsSum / ghostCoinsList.length : 0;
 
-    if ((ghostCoinsList.length < pushIndex || Math.abs(ghostCoins - ghostCoinsAVG) > 500) && ghostCoins < 1000000) {
+    if ((ghostCoinsList.length < pushIndex || Math.abs(ghostCoins.toFixed() - ghostCoinsAVG.toFixed()) > 500) && ghostCoins < 1000000) {
         if (ghostCoinsList.length < maxListSize) {
             ghostCoinsList.push(ghostCoins);
             ghostCoinsSum += ghostCoins;
