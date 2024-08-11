4b39af8e74754e01599eb111b2b571f9e2564ec7
RolexDE <133770982+RolexDE@users.noreply.github.com>
2024-08-11 18:44:23 +0200

    refactor: Update checkData.js to improve module loading and backtracking functionality

--- utils/checkData.js
+++ utils/checkData.js
@@ -23,6 +23,11 @@ register("step", () => {
         if (checkAllCriteria()) {
             dataLoaded = true;
             ChatLib.chat("§6[SBO] §4Module Loaded");
+            unlockAchievement(38); // SBO downloaded
+            if (data.backTrack == false) {
+                new TextComponent("&6[SBO] &aDo you want to backtrack your Diana Achievements?").setClick("run_command", "/sbobacktrackachivements").setHover("show_text", "&7Click to backtrack your Achievements").chat();
+                data.backTrack = true;
+            }
         }
     }
 }).setFps(1);
@@ -49,11 +54,6 @@ function checkAllCriteria() {
     }
     // print("check1: " + check1 + " check2: " + check2 + " check3: " + check3 + " check4: " + check4 + " check5: " + check5 + " check6: " + check6 + " check7: " + check7 + " check8: " + check8 + " check9: " + check9);
     if (check1 && check2 && check3 && check4 && check5 && check6 && check7 && check8 && check9) {
-        unlockAchievement(38); // SBO downloaded
-        if (data.backTrack == false) {
-            new TextComponent("&6[SBO] &aDo you want to backtrack your Diana Achievements?").setClick("run_command", "/sbobacktrackachivements").setHover("show_text", "&7Click to backtrack your Achievements").chat();
-            data.backTrack = true;
-        }
         return true;
     }
     return false;
