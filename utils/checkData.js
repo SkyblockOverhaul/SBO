4acbc90496fa653387cecc2daaf256dd33cd0719
RolexDE <133770982+RolexDE@users.noreply.github.com>
2024-08-12 03:09:31 +0200

    feat: Improve data loading logic in checkData.js

--- utils/checkData.js
+++ utils/checkData.js
@@ -18,17 +18,16 @@ function checkDataLoaded() {
     }
 }
 
-register("step", () => {
-    if (!dataLoaded) {
-        if (checkAllCriteria()) {
-            dataLoaded = true;
-            ChatLib.chat("§6[SBO] §4Module Loaded");
-            unlockAchievement(38); // SBO downloaded
-            if (data.backTrack == false && settings.achievementEnabler) {
-                new TextComponent("&6[SBO] &aDo you want to backtrack your Diana Achievements?").setClick("run_command", "/sbobacktrackachivements").setHover("show_text", "&7Click to backtrack your Achievements").chat();
-                data.backTrack = true;
-            }
+let dataLoadReg = register("step", () => {
+    if (checkAllCriteria()) {
+        dataLoaded = true;
+        ChatLib.chat("§6[SBO] §4Module Loaded");
+        unlockAchievement(38); // SBO downloaded
+        if (data.backTrack == false && settings.achievementEnabler) {
+            new TextComponent("&6[SBO] &aDo you want to backtrack your Diana Achievements?").setClick("run_command", "/sbobacktrackachivements").setHover("show_text", "&7Click to backtrack your Achievements").chat();
+            data.backTrack = true;
         }
+        dataLoadReg.unregister();
     }
 }).setFps(1);
 
