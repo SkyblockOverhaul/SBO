9a4c13b8bfe6ebabc3659495e001d3f362039283
Saotzuri <133770938+Saotzuri@users.noreply.github.com>
2024-08-18 01:33:57 +0200

    chore: Update version to 0.3.6 and fix achievement page bug

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ register("worldLoad", () => {
 });
 
 // dowload msg beispiel
-const newVersion = "0.3.5" // hier neue version eintragen wenn changelog angezeigt werden soll
+const newVersion = "0.3.6" // hier neue version eintragen wenn changelog angezeigt werden soll
 const downloadMsgReg = register("step", () => {
     if (!World.isLoaded()) return
     if (!isDataLoaded()) return
@@ -89,11 +89,7 @@ const changeLogReg = register("step", () => {
     ChatLib.chat(ChatLib.getChatBreak("&b-"))
     ChatLib.chat(`&6[SBO] &r&bVersion &e${newVersion}&r`)
     ChatLib.chat(`&aChangelog:`)
-    ChatLib.chat(`&7> &a--Diana--`)
-    ChatLib.chat(`&7> &aFixed Diana detection not working when shes minister`)
-    ChatLib.chat(`&7> &a--Other--`)
-    ChatLib.chat(`&7> &aAdded new Achivements`)
-    ChatLib.chat(`&7> &aFixed some gui texts not showing in /sboguis`)
+    ChatLib.chat(`&7> &aFixed achievement page not working as intended when you have 0 achievements unlocked`)
     ChatLib.chat(ChatLib.getChatBreak("&b-"))
 
     data.changelogVersion = newVersion
