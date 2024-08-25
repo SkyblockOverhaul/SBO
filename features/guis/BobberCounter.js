20499bd160fae8f7d4e6acae2dbd9349da92a119
RolexDE <133770982+RolexDE@users.noreply.github.com>
2024-08-25 15:51:56 +0200

    refactor: Update "bobberCounter" to "bobberOverlay" in BobberCounter.js

--- features/guis/BobberCounter.js
+++ features/guis/BobberCounter.js
@@ -13,4 +13,4 @@ const EntityFishHook = Java.type("net.minecraft.entity.projectile.EntityFishHook
 registerWhen(register('step', () => {
     bobberCount = World.getAllEntitiesOfType(EntityFishHook).filter(dist => dist.distanceTo(Player.getPlayer()) < 31).length
     bobberOverlay.setLines([bobberOverlayText.setText(`${YELLOW}${BOLD}Bobber: ${AQUA}${BOLD}${bobberCount}`)]);
-}).setFps(1), () => settings.bobberCounter);
+}).setFps(1), () => settings.bobberOverlay);
