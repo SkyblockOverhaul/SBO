596305299cd9acbff6e7b2ace57391c1a3168367
Saotzuri <133770938+Saotzuri@users.noreply.github.com>
2024-11-08 01:16:20 +0100

    Refactor Diana burrow warp logic and optimize task scheduling

--- features/Diana/ComboEpearlHelper.js
+++ features/Diana/ComboEpearlHelper.js
@@ -3,113 +3,68 @@ import { registerWhen } from "../../utils/variables";
 import { checkDiana } from "../../utils/checkDiana";
 import { getWorld } from "../../utils/world";
 import { SboOverlay, OverlayTextLine, OverlayButton, hoverText } from "../../utils/overlays";
-
-register("actionBar", (before, gain, type, amount, next, after) => {
-    if (type !== "Combat" || combo.expired) return;
-    timer.setComboType(combo.type);
-    if (!combo.expired) timer.start();
-}).setCriteria("${before}+${gain} ${type} (${amount}/${next})${after}");
-
-register("chat", (type, trash) => {
-    combo.type = parseInt(type);
-    if (!combo.type) return;
-    combo.expired = false;
-    timer.setComboType(combo.type);
-    timer.start();
-    ChatLib.chat(combo.type);
-    ChatLib.chat(combo.expired);
-}).setCriteria("${type} Kill Combo ${trash}");
-
-register("chat", (amount) => {
-    combo.expired = true;
-    timer.stop();
-    ChatLib.chat(combo.expired);
-}).setCriteria("&r&cYour Kill Combo has expired! You reached a ${amount} Kill Combo!&r");
+import Countdown from "../../utils/Countdown";
 
 class Combo {
     constructor() {
         this.type = 0;
         this.expired = false;
     }
-}
-
-class Timer {
-    constructor(time, autostart = false, isCountdown = true) {
-        this.defaultTime = time * 20;
-        this.time = this.defaultTime;
-        this.currenttime = isCountdown ? this.time : 0;
-        this.ticks = isCountdown ? this.time : 0;
-        this.isCountdown = isCountdown;
-        this.tickEvent = null;
-        this.active = false;
-
-        if (autostart) this.start();
-
-        register("worldUnload", () => {
-            this.stop();
-        });
+    
+    setType(type) {
+        this.type = type;
+        this.expired = false;
     }
 
-    setComboType(comboType) {
-        switch(comboType) {
-            case 5:
-                this.time = 7.5 * 20;
-                break;
-            case 10:
-                this.time = 5.9 * 20;
-                break;
-            case 15:
-                this.time = 4.5 * 20;
-                break;
-            case 20:
-                this.time = 3.5 * 20;
-                break;
-            case 25:
-                this.time = 2.9 * 20;
-                break;
-            case 30:
-                this.time = 1.5 * 20;
-                break;
-            default:
-                this.active = false;
-                return;
-        }
-        this.currenttime = this.time;
-        this.ticks = this.time;
-        this.active = true;
-    }
-
-    start() {
-        this.stop();
-        this.ticks = this.time;
-        this.registerTicks();
-        this.active = true;
-    }
-
-    registerTicks() {
-        this.tickEvent = register("tick", () => {
-            if (!this.active) return;
-
-            this.isCountdown ? this.ticks-- : this.ticks++;
-
-            if ((this.isCountdown && this.ticks <= 0) || (!this.isCountdown && this.ticks >= this.time)) {
-                this.stop();
-                console.log("Timer gestoppt");
-            } else {
-                console.log(`Timer: ${(this.ticks / 20).toFixed(3)} Sekunden`);
-            }
-        });
-    }
-
-    stop() {
-        if (this.tickEvent) {
-            this.tickEvent.unregister();
-            this.tickEvent = null;
-        }
-        this.active = false;
-        console.log("Timer gestoppt");
+    expire() {
+        this.expired = true;
     }
 }
 
 let combo = new Combo();
-let timer = new Timer(0, false, true);
+let countdownTimer = null;
+
+function startCountdownTimer(comboType) {
+    let time;
+    switch (comboType) {
+        case 5: time = 7.5; break;
+        case 10: time = 5.9; break;
+        case 15: time = 4.5; break;
+        case 20: time = 3.5; break;
+        case 25: time = 2.9; break;
+        case 30: time = 1.5; break;
+        default: combo.expired = true; return;
+    }
+
+    if (countdownTimer) {
+        countdownTimer.stop();
+    }
+
+    countdownTimer = new Countdown(
+        time,
+        (remainingTime) => console.log(`Timer: ${remainingTime} seconds`),
+        () => {
+            console.log("Countdown beendet");
+            combo.expire();
+        }
+    );
+    
+    countdownTimer.start();
+}
+
+register("actionBar", (before, gain, type, amount, next, after) => {
+    if (type !== "Combat" || combo.expired) return;
+    startCountdownTimer(combo.type);
+}).setCriteria("${before}+${gain} ${type} (${amount}/${next})${after}");
+
+register("chat", (type, trash) => {
+    combo.setType(parseInt(type));
+    startCountdownTimer(combo.type);
+    ChatLib.chat(`Combo Type: ${combo.type}, Expired: ${combo.expired}`);
+}).setCriteria("${type} Kill Combo ${trash}");
+
+register("chat", (amount) => {
+    combo.expire();
+    if (countdownTimer) countdownTimer.stop();
+    ChatLib.chat(`Combo Expired: ${combo.expired}`);
+}).setCriteria("&r&cYour Kill Combo has expired! You reached a ${amount} Kill Combo!&r");
