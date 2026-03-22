c45545741f8bf69b4eb9695ce3b6345a0279b6cc
Saotzuri <133770938+Saotzuri@users.noreply.github.com>
2026-03-22 20:34:01 +0100

    Update WarpPoint.kt

--- src/main/kotlin/net/sbo/mod/utils/waypoint/WarpPoint.kt
+++ src/main/kotlin/net/sbo/mod/utils/waypoint/WarpPoint.kt
@@ -15,8 +15,7 @@ data class WarpPoint(
 )
 
 val hubWarps: Map<String, WarpPoint> = mapOf(
-    "hub" to WarpPoint(SboVec(0.50, 77.00, -0.50), true),
-    "museum" to WarpPoint(SboVec(29.50, 72.00, 1.50), true)
+    "hub" to WarpPoint(SboVec(0.50, 77.00, -0.50), true)
 )
 
 val additionalHubWarps: Map<String, WarpPoint> = mapOf(
@@ -25,7 +24,8 @@ val additionalHubWarps: Map<String, WarpPoint> = mapOf(
     "crypt" to WarpPoint(SboVec(-160.50, 62.00, -106.50), true, "cryptWarp"),
     "stonks" to WarpPoint(SboVec(-36.50, 70.00, -81.50), true, "stonksWarp"),
     "da" to WarpPoint(SboVec(91.50, 75.00, 173.50), true, "darkAuctionWarp"),
-    "taylor" to WarpPoint(SboVec(29.50, 73.00, -41.50), true, "taylorWarp")
+    "taylor" to WarpPoint(SboVec(29.50, 73.00, -41.50), true, "taylorWarp"),
+    "museum" to WarpPoint(SboVec(29.50, 72.00, 1.50), true, "museumWarp")
 )
 
 enum class AdditionalHubWarps {
@@ -34,5 +34,6 @@ enum class AdditionalHubWarps {
     CRYPT,
     STONKS,
     DA,
-    TAYLOR
+    TAYLOR,
+    Museum
 }
