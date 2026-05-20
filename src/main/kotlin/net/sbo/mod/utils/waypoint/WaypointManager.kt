01f8b4a686731c3206b05538470b12e24daab8db
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2026-05-20 18:09:03 +0300

    Dont render waypoints out of hub
    The waypoints are not cleared, they will re-appear once joining hub.
    
    Some people consider showing the waypoints on other islands, e.g, nether, is a bug of mod thinking you are in hub/mythological ritual active island, but the check was never there to start with, so more of an enhancement rather than bug fix.

--- src/main/kotlin/net/sbo/mod/utils/waypoint/WaypointManager.kt
+++ src/main/kotlin/net/sbo/mod/utils/waypoint/WaypointManager.kt
@@ -15,6 +15,7 @@ import net.sbo.mod.utils.Helper
 import net.sbo.mod.utils.Helper.checkDiana
 import net.sbo.mod.utils.Helper.sleep
 import net.sbo.mod.utils.Player
+import net.sbo.mod.utils.World
 import net.sbo.mod.utils.SoundHandler.playCustomSound
 import net.sbo.mod.utils.events.Register
 import net.sbo.mod.utils.events.annotations.SboEvent
@@ -206,6 +207,10 @@ object WaypointManager {
      * @param context The world render context.
      */
     fun renderAllWaypoints(context: WorldRenderContext) {
+        if (World.getWorld() != "Hub") {
+            return
+        }
+
         this.forEachWaypoint { waypoint ->
             waypoint.render(context)
         }
