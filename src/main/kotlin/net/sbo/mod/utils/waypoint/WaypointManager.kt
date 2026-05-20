76a4cd173181d7abab324fb0ba91aa0620312b52
Mustafa <mustafaoncel92@gmail.com>
2026-05-20 18:19:05 +0300

    Remove duplicate import oops

--- src/main/kotlin/net/sbo/mod/utils/waypoint/WaypointManager.kt
+++ src/main/kotlin/net/sbo/mod/utils/waypoint/WaypointManager.kt
@@ -15,7 +15,6 @@ import net.sbo.mod.utils.Helper
 import net.sbo.mod.utils.Helper.checkDiana
 import net.sbo.mod.utils.Helper.sleep
 import net.sbo.mod.utils.Player
-import net.sbo.mod.utils.game.World
 import net.sbo.mod.utils.SoundHandler.playCustomSound
 import net.sbo.mod.utils.events.Register
 import net.sbo.mod.utils.events.annotations.SboEvent
