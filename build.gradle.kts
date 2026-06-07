1518729cb3179b7de29862871d24f3eb21ad6fb6
Mustafa <mustafaoncel92@gmail.com>
2026-06-07 19:53:10 +0300

    Update Kotlin serialization plugin version to 2.4.0
    Signed-off-by: Mustafa <mustafaoncel92@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ import java.lang.module.ModuleDescriptor.Version
 plugins {
     java
     kotlin("jvm")
-    kotlin("plugin.serialization") version "2.3.21"
+    kotlin("plugin.serialization") version "2.4.0"
     id("net.fabricmc.fabric-loom-remap") version "1.16.2"
     id("dev.deftu.gradle.multiversion")
     id("dev.deftu.gradle.tools.bloom")
