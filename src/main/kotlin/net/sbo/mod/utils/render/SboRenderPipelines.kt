628de15818b5c75e378c1c2968e2228ab90076b8
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2026-06-20 23:47:22 +0300

    Cleanup unused RenderLayers and RenderPipelines
    This makes upgrading to new Minecraft versions simpler since we need to maintain less custom RenderLayers and RenderPipelines.
    
    The "throughWalls" and "phase" parameters passed were effectively always true, since they were all called from renderWaypoint, and Waypoint.kt always passed the throughWalls parameter as true, so the non-through walls variants of the render layers and pipelines were effectively dead code and unnecessary maintenance burden. Marked functions not called outside the class and only inside renderWaypoint as private as well.

--- src/main/kotlin/net/sbo/mod/utils/render/SboRenderPipelines.kt
+++ src/main/kotlin/net/sbo/mod/utils/render/SboRenderPipelines.kt
@@ -9,31 +9,6 @@ import net.sbo.mod.SBOKotlin
 
 /** Add new pipelines to [net.sbo.mod.compat.IrisCompatibility] */
 object SboRenderPipelines {
-    val FILLED_BOX_THROUGH_WALLS: RenderPipeline = RenderPipelines.register(
-        RenderPipeline.builder(RenderPipelines.DEBUG_FILLED_SNIPPET)
-            .withLocation(SBOKotlin.id("pipeline/debug_filled_box_through_walls"))
-            //#if MC >= 26.2
-            //$ .withVertexBinding(0, DefaultVertexFormat.POSITION_COLOR)
-            //$ .withPrimitiveTopology(PrimitiveTopology.TRIANGLE_STRIP)
-            //#else
-            .withVertexFormat(DefaultVertexFormat.POSITION_COLOR, Mode.TRIANGLE_STRIP)
-            //#endif
-            .build()
-    )
-
-    val LINES: RenderPipeline = RenderPipelines.register(
-        RenderPipeline.builder(RenderPipelines.LINES_SNIPPET)
-            .withLocation(SBOKotlin.id("pipeline/line_strip"))
-            //#if MC >= 26.2
-            //$ .withVertexBinding(0, DefaultVertexFormat.POSITION_COLOR_NORMAL_LINE_WIDTH)
-            //$ .withPrimitiveTopology(PrimitiveTopology.LINES)
-            //#else
-            .withVertexFormat(DefaultVertexFormat.POSITION_COLOR_NORMAL_LINE_WIDTH, Mode.LINES)
-            //#endif
-            .withCull(false)
-            .build()
-    )
-
     val LINES_THROUGH_WALLS: RenderPipeline = RenderPipelines.register(
         RenderPipeline.builder(RenderPipelines.LINES_SNIPPET)
             .withLocation(SBOKotlin.id("pipeline/line_through_walls"))
@@ -48,16 +23,8 @@ object SboRenderPipelines {
             .build()
     )
 
-    val BEACON_BEAM_OPAQUE: RenderPipeline = RenderPipeline.builder(RenderPipelines.BEACON_BEAM_SNIPPET)
-        .withLocation(SBOKotlin.id("beacon_beam_opaque"))
-        .build()
-
     val BEACON_BEAM_OPAQUE_THROUGH_WALLS: RenderPipeline = RenderPipeline.builder(RenderPipelines.BEACON_BEAM_SNIPPET)
         .withLocation(SBOKotlin.id("beacon_beam_opaque_through_walls"))
-        .build()
-
-    val BEACON_BEAM_TRANSLUCENT: RenderPipeline = RenderPipeline.builder(RenderPipelines.BEACON_BEAM_SNIPPET)
-        .withLocation(SBOKotlin.id("beacon_beam_translucent"))
         .build()
 
     val BEACON_BEAM_TRANSLUCENT_THROUGH_WALLS: RenderPipeline = RenderPipeline.builder(RenderPipelines.BEACON_BEAM_SNIPPET)
