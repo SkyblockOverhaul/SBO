4c32a21a2432ddcad5d4cd87ef8136cd0c72a7f2
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2026-07-15 20:37:22 +0300

    Bump to 500ms

--- src/main/kotlin/net/sbo/mod/diana/achievements/AchievementManager.kt
+++ src/main/kotlin/net/sbo/mod/diana/achievements/AchievementManager.kt
@@ -276,7 +276,7 @@ object AchievementManager {
 
     @SboEvent
     fun trackBeKills(event: GuiOpenEvent) {
-        Helper.sleep(250) {
+        Helper.sleep(500) {
             val screen = event.screen
             if (screen !is AbstractContainerScreen<*>) return@sleep
             if (!event.screen.title.string.contains("Mythological Creatur", ignoreCase = true)) return@sleep
