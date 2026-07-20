b30f320fbab002e7cf9477735f0b18ebcc1db024
Saotzuri <133770938+Saotzuri@users.noreply.github.com>
2026-07-20 16:22:34 +0200

    Use elected mayor year in medal clip titles
    Update `MedalIntegration` to use `Mayor.mayorElectedYear` instead of `Mayor.sbYear` when generating clip titles. This aligns saved Minos relic clip labels with the elected mayor year context.

--- src/main/kotlin/net/sbo/mod/utils/medal/MedalIntegration.kt
+++ src/main/kotlin/net/sbo/mod/utils/medal/MedalIntegration.kt
@@ -4,8 +4,7 @@ import net.sbo.mod.SBOKotlin
 import net.sbo.mod.settings.categories.Diana
 import net.sbo.mod.utils.chat.Chat
 import net.sbo.mod.utils.data.SboDataObject.dianaTrackerMayor
-import net.sbo.mod.utils.game.Mayor
-import net.sbo.mod.utils.game.Mayor.sbYear
+import net.sbo.mod.utils.game.Mayor.mayorElectedYear
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -87,7 +86,7 @@ object MedalIntegration {
     fun saveMinosRelicClip() = triggerEvent(minosRelicEvent)
 
     private fun clipTitle(dropName: String, dropNumber: Int): String {
-        return "[${sbYear} YEAR] $dropName #$dropNumber"
+        return "[${mayorElectedYear} YEAR] $dropName #$dropNumber"
     }
 
     private fun displayTitle(dropName: String, dropNumber: Int): String {
