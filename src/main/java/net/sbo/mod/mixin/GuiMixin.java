8663477a6e6ec84998051971f63bd2825fc9a575
Mustafa <mustafaoncel92@gmail.com>
2026-07-21 06:07:37 +0300

    Change mixin target from Minecraft to Gui
    Signed-off-by: Mustafa <mustafaoncel92@gmail.com>

--- src/main/java/net/sbo/mod/mixin/GuiMixin.java
+++ src/main/java/net/sbo/mod/mixin/GuiMixin.java
@@ -1,6 +1,6 @@
 package net.sbo.mod.mixin;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.screens.Screen;
 import net.sbo.mod.utils.events.SBOEvent;
 import net.sbo.mod.utils.events.impl.guis.GuiOpenEvent;
@@ -11,10 +11,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
-public abstract class MinecraftMixin {
+@Mixin(Gui.class)
+final class GuiMixin {
     @Inject(method = "setScreen", at = @At("HEAD"))
-    public void onSetScreen(@Nullable final Screen screen, @NonNull final CallbackInfo ci) {
+    private final void sbo$onSetScreen(@Nullable final Screen screen, @NonNull final CallbackInfo ci) {
         if (screen != null) {
             SBOEvent.INSTANCE.emit(new GuiOpenEvent(screen, ci));
         }
