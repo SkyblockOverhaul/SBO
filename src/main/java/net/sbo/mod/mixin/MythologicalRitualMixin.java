7b4a0529484fa0dffe376646f48495459983fe81
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2026-08-28 12:26:50 +0300

    Auto disable Skyblocker and Firmament diana features if SBO's ones are enabled

---
+++ src/main/java/net/sbo/mod/mixin/MythologicalRitualMixin.java
@@ -0,0 +1,25 @@
+package net.sbo.mod.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.sbo.mod.settings.categories.Diana;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+
+/**
+ * If you are a Skyblocker developer and have an issue with this mixin's existence do not hesitate to make a pull request.
+ *
+ * Reasoning: Users report Skyblocker persisting lots of orange burrow waypoints even after fully digging them and they often
+ * blame SBO until we convince them that it's Skyblocker that they need to disable. This ensures that Skyblocker won't get in the way
+ * if the user choose to use the SBO's Arrow Guess option which is even more powerful than Skyblocker's implementation.
+ *
+ * If the SBO's Arrow Guess is false, the original isActive makes Skyblocker behave normally.
+ */
+@Pseudo
+@Mixin(targets = "de.hysky.skyblocker.skyblock.waypoint.MythologicalRitual", remap = false)
+final class MythologicalRitualMixin {
+    @ModifyReturnValue(method = "isActive", at = @At("RETURN"))
+    private static final boolean sbo$disableSkyblockerMythologicalRitualHelperIfSBOEnabled(final boolean isActive) {
+        return isActive && !Diana.INSTANCE.arrowGuess;
+    }
+}
---
+++ src/main/java/net/sbo/mod/mixin/NearbyBurrowsSolverMixin.java
@@ -0,0 +1,46 @@
+package net.sbo.mod.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.sbo.mod.settings.categories.Diana;
+import org.jspecify.annotations.NonNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Coerce;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * If you are a Firmament developer and have an issue with this mixin's existence do not hesitate to make a pull request.
+ *
+ * Reasoning: Users frequently report white boxed waypoints and a white line pointing to it under the ground typically when/after
+ * using an Abiphone, and in other cases sometimes in the air as well. It seems Firmament does not verify that the guess is in a grass block,
+ * which we do and so know that these waypoints are not from SBO, but users do not know this and frequently ask us.
+ *
+ * If the SBO's Arrow Guess is false, the original isEnabled makes Firmament behave normally.
+ */
+@Pseudo
+@Mixin(targets = "moe.nea.firmament.features.diana.NearbyBurrowsSolver", remap = false)
+final class NearbyBurrowsSolverMixin {
+    // no isEnabled() in this class, accesses config field directly, kind of hacky way to ci.cancel them but whatever
+    @Inject(method = "onParticles", at = @At("HEAD"), cancellable = true)
+    private final void sbo$disableNearbyBurrowsSolverOnParticlesIfSBOEnabled(@NonNull @Coerce final Object event, @NonNull final CallbackInfo ci) {
+        if (Diana.INSTANCE.closeBurrowDetection) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "onRender", at = @At("HEAD"), cancellable = true)
+    private final void sbo$disableNearbyBurrowsSolverOnRenderIfSBOEnabled(@NonNull @Coerce final Object event, @NonNull final CallbackInfo ci) {
+        if (Diana.INSTANCE.closeBurrowDetection) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "onBlockClick", at = @At("HEAD"), cancellable = true)
+    private final void sbo$disableNearbyBurrowsSolverOnBlockClickIfSBOEnabled(@NonNull final BlockPos blockPos, @NonNull final CallbackInfo ci) {
+        if (Diana.INSTANCE.closeBurrowDetection) {
+            ci.cancel();
+        }
+    }
+}
