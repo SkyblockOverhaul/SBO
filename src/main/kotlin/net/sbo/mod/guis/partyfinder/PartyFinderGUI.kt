5131140d8623f940174b782a460318992d601e77
Mustafa ÖNCEL <mustafaoncel92@gmail.com>
2026-09-26 00:10:51 +0300

    Remove PF Text Scale option

--- src/main/kotlin/net/sbo/mod/guis/partyfinder/PartyFinderGUI.kt
+++ src/main/kotlin/net/sbo/mod/guis/partyfinder/PartyFinderGUI.kt
@@ -149,8 +149,8 @@ class PartyFinderGUI : WindowScreen(ElementaVersion.V10) {
     }
 
     internal fun getTextScaleOfScaleText(base: Float = 1f): PixelConstraint {
-        return if (base + PartyFinder.scaleText <= 0f) 0.1f.pixels()
-        else (base + PartyFinder.scaleText).pixels()
+        return if (base <= 0f) 0.1f.pixels()
+        else base.pixels()
     }
 
     internal fun getFilter(pageType: String, callback: (((Party) -> Boolean)?) -> Unit) {
